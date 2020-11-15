57bf16eb5e3642fd080e4f2bc14a3b3f3e265960
Alekslov <Alekslov@users.noreply.github.com>
2020-11-15 22:13:53 +0200

    tren

---
+++ main.js
@@ -0,0 +1,19 @@
+// Laura megsta geles
+const vardas = 'Laura';
+const objektas = 'geles';
+const sakinys = vardas + ' megsta ' + objektas;
+console.log(sakinys);
+
+
+
+// CHuck Norris (80)
+const name1 = 'Chuck';
+const name2 = 'Norris';
+const birth = 1940;
+const date = 2020;
+const suma = date - birth;
+const sakinys2 = name1 + ' ' + name2 + ' (' + suma + ')';
+console.log(sakinys2);
+
+const fraze = 'Jis pasake: "Labas".';
+console.log(fraze);
